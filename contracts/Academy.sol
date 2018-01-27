e0d6c79232ddc959da5ba059c7710a5dded4d89a
10thfloor <remember.mackenzie@gmail.com>
2018-01-27 09:06:35 -0800

    Install Open Zeppelin, implement academy ownership.

--- contracts/Academy.sol
+++ contracts/Academy.sol
@@ -1,6 +1,8 @@
-pragma solidity ^0.4.16;
+pragma solidity ^0.4.17;
 
-contract Academy {
+import 'zeppelin-solidity/contracts/ownership/Claimable.sol';
+
+contract Academy is Claimable {
     
     struct Student {
         bool enrolled;
@@ -17,18 +19,28 @@ contract Academy {
         name = _name;
     }
 
-    function enroll() public {
+    function enroll() public onlyOwner {
         require(students[msg.sender].enrolled == false);
         students[msg.sender] = Student({ enrolled: true });
 
         Enroll(msg.sender);
     }
 
-    function graduate(address _student) public {
+    function graduate(address _student) public onlyOwner {
         require(students[_student].enrolled == true);
         graduates[msg.sender] = students[_student];
         delete students[_student];
 
         Graduate(msg.sender);
+    }
+
+    function didAttend(address _student) public view returns (Student) {
+        require(_student != address(0));
+        return students[_student];
+    }
+
+    function didGraduate(address _student) public view returns (Student) {
+        require(_student != address(0));
+        return graduates[_student];
     }
 }
