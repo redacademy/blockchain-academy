9151b6a4ea407767b0378116f0d29b880864ad79
10thfloor <remember.mackenzie@gmail.com>
2018-01-27 21:59:02 -0800

    Add tests.

--- contracts/Academy.sol
+++ contracts/Academy.sol
@@ -3,30 +3,39 @@ pragma solidity ^0.4.17;
 import 'zeppelin-solidity/contracts/ownership/Claimable.sol';
 
 contract Academy is Claimable {
+
+    event Enroll(address student);
+    event Graduate(address student);
     
     struct Student {
         bool enrolled;
     }
     
-    string public name;
+    bytes32 public name;
     mapping(address => Student) public students;
     mapping(address => Student) public graduates;
     
-    event Enroll(address student);
-    event Graduate(address student);
     
-    function Academy(string _name) public {
+    function Academy(bytes32 _name) 
+        public 
+    {
         name = _name;
     }
 
-    function enroll() public onlyOwner {
+    function enroll() 
+        public 
+        onlyOwner 
+    {
         require(students[msg.sender].enrolled == false);
         students[msg.sender] = Student({ enrolled: true });
 
         Enroll(msg.sender);
     }
 
-    function graduate(address _student) public onlyOwner {
+    function graduate(address _student) 
+        public 
+        onlyOwner 
+    {
         require(students[_student].enrolled == true);
         graduates[msg.sender] = students[_student];
         delete students[_student];
@@ -34,13 +43,31 @@ contract Academy is Claimable {
         Graduate(msg.sender);
     }
 
-    function didAttend(address _student) public view returns (Student) {
+    function didAttend(address _student) 
+        public 
+        view 
+        returns (Student) 
+    {
         require(_student != address(0));
         return students[_student];
     }
 
-    function didGraduate(address _student) public view returns (Student) {
+    function didGraduate(address _student) 
+        public 
+        view 
+        returns (Student) 
+    {
         require(_student != address(0));
         return graduates[_student];
     }
+
+    function close() 
+        public 
+        onlyOwner 
+    {
+        selfdestruct(owner);
+    }
+    // Fallback function in case someone sends ether to the contract so it doesn't get lost.
+    function() payable {}
 }
+
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var Academy = artifacts.require("Academy");
+var AcademyFactory = artifacts.require("AcademyFactory");
 
 module.exports = function(deployer) {
+  deployer.deploy(AcademyFactory);
   deployer.deploy(Academy);
 };
